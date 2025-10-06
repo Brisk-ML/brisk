7ef5f47c8e433a787983c23a1d03a6b31ed72909
BFieguth <bfieguth@uoguelph.ca>
2025-10-06 14:56:29 -0400

    refactor: disable old test suite to refactor from clean start

--- tests/e2e/e2e_binary/metrics.py
+++
@@ -1,4 +0,0 @@
-# metrics.py
-from tests.e2e import e2e_setup
-                
-METRIC_CONFIG = e2e_setup.metric_config()
--- tests/e2e/e2e_regression/data.py
+++
@@ -1,6 +0,0 @@
-# data.py
-from brisk.data.data_manager import DataManager                
-
-BASE_DATA_MANAGER = DataManager(
-    test_size = 0.2
-)              
