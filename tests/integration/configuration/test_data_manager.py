03cbcb83ebda182dabd00a89bf18b08f71dd1641
BFieguth <bfieguth@uoguelph.ca>
2025-11-14 15:06:42 -0500

    tests: rewrite DataManager unit tests

---
+++ tests/integration/configuration/test_data_manager.py
@@ -0,0 +1,21 @@
+"""Integration tests for DataManager."""
+
+class TestDataManagerIntegration:
+    def test_split_no_table_name(self):
+        pass
+
+    def test_split_table_name(self):
+        pass
+
+    def test_split_0_splits(self):
+        pass
+
+    def test_split_1_split(self):
+        pass
+
+    def test_split_2_splits(self):
+        pass
+
+    def test_split_drop_group_column(self):
+        pass
+
---
+++ tests/integration/configuration/test_project.py
@@ -0,0 +1,11 @@
+"""Integration tests for find_project_root."""
+
+class TestFindProjectRootIntegration:
+    def test_in_current_dir(self):
+        pass
+
+    def test_in_parent_dir(self):
+        pass
+
+    def test_in_child_dir(self):
+        pass
